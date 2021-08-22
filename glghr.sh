24000b2bae18b31bc142913f4808431a0058c725
Kwitsch <kwitsch@gmail.com>
2021-08-23 01:25:56 +0200

    fallback on tags if no official releases availabla

--- glghr.sh
+++ glghr.sh
@@ -6,7 +6,12 @@ echo "download repo: $user/$repo"
 file="${repo}.tar.gz"
 baseurl="https://api.github.com/repos/$user/$repo/releases/latest"
 echo "api url: $baseurl"
-dlurl=$(curl -s ${baseurl} | grep "tarball_url.*" | cut -d : -f 2,3 | tr -d '\"' | tr -d ',' | tr -d ' ')
+dlurl=$(curl -s ${baseurl} | grep "tarball_url.*" | head -1 | cut -d : -f 2,3 | tr -d '\"' | tr -d ',' | tr -d ' ')
+if [ -z "$dlurl" ]; then
+    baseurl="https://api.github.com/repos/$user/$repo/tags"
+    echo "api url2: $baseurl"
+    dlurl=$(curl -s ${baseurl} | grep "tarball_url.*" | head -1 | cut -d : -f 2,3 | tr -d '\"' | tr -d ',' | tr -d ' ')
+fi
 echo "download url: $dlurl"
 curl -fsSL --retry 3 ${dlurl} -o ${file}
 mkdir ${repo}
